000b5c654f9295d2b33444e10767ec1dafff6a4e
Martin Romano <martinpromano@outlook.com>
2017-09-16 20:01:57 -0300

    pertenencias a intervalos

--- src/figuras/Punto.java
+++ src/figuras/Punto.java
@@ -28,4 +28,12 @@ public class Punto {
 	public double calcularDistanciaA(Punto punto) {
 		return Math.sqrt(Math.pow(this.x - punto.x,2) + Math.pow(this.y - punto.y,2));
 	}
+	
+	public boolean enIntervaloProyX(Punto p1,Punto p2) {
+		return p1.x <= this.x && this.x <= p2.x;
+	}
+	
+	public boolean enIntervaloProyY(Punto p1,Punto p2) {
+		return p1.y <= this.y && this.y <= p2.y;
+	}
 }
--- src/test/TestPunto.java
+++ src/test/TestPunto.java
@@ -6,13 +6,31 @@ import org.junit.Test;
 import figuras.Punto;
 
 public class TestPunto {
-	Punto p1 = new Punto(8,6);
-	Punto p2 = new Punto(4,3);
 	
 	@Test
 	public void distanciaCorrecta() {
 		//Assert.assertEquals(6, p1.calcularDistanciaA(p2),0); //6
+		Punto p1 = new Punto(8,6);
+		Punto p2 = new Punto(4,3);
 		
 		Assert.assertEquals(5, p1.calcularDistanciaA(p2),0);
+	}
+	
+	@Test
+	public void contenidoEnProyX() {
+		Punto p1 = new Punto(2,8);
+		Punto p2 = new Punto(9,4);
+		Punto p3 = new Punto(4,7);
+		
+		Assert.assertEquals(true, p3.enIntervaloProyX(p1, p2));
+	}
+	
+	@Test
+	public void contenidoEnProyY() {
+		Punto p1 = new Punto(2,8);
+		Punto p2 = new Punto(9,4);
+		Punto p3 = new Punto(4,7);
+		
+		Assert.assertEquals(true, p3.enIntervaloProyY(p2, p1));
 	}
 }
